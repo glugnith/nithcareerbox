35a0706ad0949b684d2d41d94b765eeac189b290
tanzin <ricchog@gmail.com>
2016-07-14 22:40:58 +0530

    added captcha to login and signup form

--- login.php
+++ login.php
@@ -10,57 +10,73 @@ if (!$con)
   	}
 	if (isset($_POST['loginform']))
 	{
-   	// login related check
-		//get value
-		$username = $_POST['username'];
-		$regpwd2 = $_POST['password'];
-		$password = md5($regpwd2);
-		//fetching data from database
-		$result = mysqli_query($con,"SELECT * FROM user WHERE username = '$username' AND password = '$password'");
-		//validate
-		//checking if database retuns any rows
-		$out = mysqli_fetch_array($result);
-		$active = $out['active'];
-		$count = 0;
-		$count = mysqli_num_rows($result);
-		
-		if($count > 0 && $active == 1)
+		//captcha validation
+		if(empty($_SESSION['6_letters_code'] ) ||
+		strcasecmp($_SESSION['6_letters_code'], $_POST['6_letters_code']) != 0)
 		{
-			$_SESSION['user'] = $username;
-			header("Location: index.php");
+		//Note: the captcha code is compared case insensitively.
+		//if you want case sensitive match, update the check above to
+			$wrongpwd = "The captcha code does not match!";
 		}
-		else if( $count > 0 && $active == 0 )
-			$wrongpwd = "Ask admin to activate your account.";
-		else
-			$wrongpwd = "Either username or password is wrong.";
+	   	if(empty($wrongpwd)) {
+			// login related check
+
+			//get value
+			$username = $_POST['username'];
+			$regpwd2 = $_POST['password'];
+			$password = md5($regpwd2);
+			//fetching data from database
+			$result = mysqli_query($con,"SELECT * FROM user WHERE username = '$username' AND password = '$password'");
+			//validate
+			//checking if database retuns any rows
+			$out = mysqli_fetch_array($result);
+			$active = $out['active'];
+			$count = 0;
+			$count = mysqli_num_rows($result);
+			
+			if($count > 0 && $active == 1)
+			{
+				$_SESSION['user'] = $username;
+				header("Location: index.php");
+			}
+			else if( $count > 0 && $active == 0 )
+				$wrongpwd = "Ask admin to activate your account.";
+			else
+				$wrongpwd = "Either username or password is wrong.";
+   		}
 	} 
 	//registration form section..................
 	if (isset($_POST['registerform']))
 	{
-	   // registration related check
-		$regusername = $_POST['regusername'];
-		$regfullname = $_POST['regfullname'];
-		$regemail    = $_POST['regemail'];
-		$regpwd1     = $_POST['regpwd1'];
-		$regpwd2     = $_POST['regpwd2'];
+		//captcha validation
+		if(empty($_SESSION['6_letters_code'] ) || strcasecmp($_SESSION['6_letters_code'], $_POST['6_letters_code']) != 0)
+		{
+			$errors = "The captcha code does not match!";
+		}
+	   	if(empty($errors)) {
+	   		// registration related check
+			$regusername = $_POST['regusername'];
+			$regfullname = $_POST['regfullname'];
+			$regemail    = $_POST['regemail'];
+			$regpwd1     = $_POST['regpwd1'];
+			$regpwd2     = $_POST['regpwd2'];
 
-		//comparing both passwords........
-		//if passoword do not match show error
-		if( !($regpwd1 == $regpwd2))
-		{
-			$unmatchedpass = "password do not match";
-		}
-		//if successful.............
-		else
-		{
-			//$a = htmlspecialchars($regfullname);
-			$regpwd1 = md5($regpwd2);
-			mysqli_query($con,"insert into user values($regusername, '$regfullname', '$regemail','$regpwd1', null ,0)");
-			$success = "Registration successful";
-			$_SESSION['MESSAGE'] = $success;
-		}
-		
-  
+			//comparing both passwords........
+			//if passoword do not match show error
+			if( !($regpwd1 == $regpwd2))
+			{
+				$unmatchedpass = "password do not match";
+			}
+			//if successful.............
+			else
+			{
+				//$a = htmlspecialchars($regfullname);
+				$regpwd1 = md5($regpwd2);
+				mysqli_query($con,"insert into user values($regusername, '$regfullname', '$regemail','$regpwd1', null ,0)");
+				$success = "Registration successful";
+				$_SESSION['MESSAGE'] = $success;
+			}
+	   	}
 	}
 
 ?>
@@ -90,13 +106,14 @@ if (!$con)
 	LogIN
 </button>
 </form>
-</div>
 <div style="text-align: center; color: green; ">
-	<h6 style="font-size: 20px; color: green;"><?php if(isset($_POST['registerform']))if(isset($sucess)) echo $sucess; ?></h6>
+	<h6 style="font-size: 20px; color: green;"><?php if(isset($_POST['registerform']))if(isset($success)) echo $success; ?></h6>
 	<h6 style="font-size: 20px; color: red;"><?php if(isset($_POST['registerform'])) if(isset($unmatchedpass)) echo $unmatchedpass; ?></h6>
+	<h6 style="font-size: 20px; color: red;"><?php if(isset($_POST['registerform'])) if(isset($errors)) echo $errors; ?></h6>
 	<h6 style="font-size: 20px; color: red;"><?php if(isset($_POST['loginform'])) if(isset($wrongpwd)) echo $wrongpwd; ?></h6>
-
 </div>
+</div>
+
 <!--Pop up sign Up form -->
 <div class="modal fade" id="signupForm">
 	<div class="modal-dialog">
@@ -142,6 +159,17 @@ if (!$con)
 							<input type="password" name="regpwd2" placeholder="repeat password" class="form-control input-sm">
 						</div>
 					</div>
+					<div class="form-group">
+						
+							<div class="col-md-offset-1 center">
+								<img src="captcha.php?rand=<?php echo rand(); ?>" id='captchaimg' >
+							</div>
+							<label class="col-md-4 col-md-offset-1" for='message'>Enter the code above here :</label><br>
+							<div class="col-md-5">
+								<input id="6_letters_code" name="6_letters_code" type="text" class="form-control input-sm" placeholder="write code here"><small>Can't read the image? click <a href='javascript: refreshCaptcha();'>here</a> to refresh</small>
+							</div>
+							
+					</div>
 
 					<div class="form-group">
 						<div class="col-md-2 col-md-offset-8">
@@ -181,6 +209,18 @@ if (!$con)
 						</div>
 					</div>
 					<div class="form-group">
+						
+							<div class="col-md-offset-1 center">
+								<img src="captcha.php?rand=<?php echo rand(); ?>" id='captchaimg' >
+							</div>
+							<label class="col-md-4 col-md-offset-1" for='message'>Enter the code above here :</label><br>
+							<div class="col-md-5">
+								<input id="6_letters_code" name="6_letters_code" type="text" class="form-control input-sm" placeholder="write code here"><small>Can't read the image? click <a href='javascript: refreshCaptcha();'>here</a> to refresh</small>
+							</div>
+							
+					</div>
+
+					<div class="form-group">
 						<div class="col-md-2 col-md-offset-8">
 							<input type="submit" class="btn btn-success" name="loginform" value="Log In" >
 						</div>
@@ -192,6 +232,17 @@ if (!$con)
 		
 	</div>
 </div>
+<!-- refresh captcha code -->
+<script>
+	function refreshCaptcha()
+	{
+		var img = document.images['captchaimg'];
+		img.src = img.src.substring(0,img.src.lastIndexOf("?"))+"?rand="+Math.random()*1000;
+	}
+</script>
+<script>
+
+</script>
 <!-- javascript files    -->
 	<script src="js/jquery.min.js"></script>
 <!-- Latest compiled and minified JavaScript -->
